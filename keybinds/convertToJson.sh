1146f1c59580cd8d981a975a99d17cc3c6ed9d6e
Anthony Cieri <penguinmillion@gmail.com>
2023-01-27 13:10:32 -0800

    Initial Commit, first time using git. *wow*

---
+++ keybinds/convertToJson.sh
@@ -0,0 +1,19 @@
+{
+    "apps" : {
+        "Open apps" : {
+            "mod" : {
+                "main"
+            },
+            "keys" : {
+                "R"
+            },
+            "command" : {
+                "exec",
+                "wofi -i --show drun"
+            }
+        },
+        "homerow left" : {
+
+        }
+    }
+}
---
+++ showKeybindings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "this is the keybindings thing"
+echo "second line"
+read
