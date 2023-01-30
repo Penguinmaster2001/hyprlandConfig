06b20359bdb714806505d06578946e3506ee54aa
Anthony Cieri <penguinmillion@gmail.com>
2023-01-29 22:38:40 -0800

    volume working

--- keybinds/notificationScript.sh
+++ keybinds/notificationScript.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+DURATION=5000
+
 case $1 in
     "mute")
         pactl set-sink-mute 3 toggle
-        notify-send -t 500 $(pactl get-sink-mute 3)
+        notify-send -t $DURATION $(pactl get-sink-mute 3)
+    ;;
+
+    "vd")
+        pactl set-sink-volume 3 -$2%
+        notify-send -t $DURATION $(pactl get-sink-volume 3 | grep -o -E " ?[0-9]?[0-9]?[0-9]%")
+    ;;
+
+    "vu")
+        pactl set-sink-volume 3 +$2%
+        notify-send -t $DURATION $(pactl get-sink-volume 3 | grep -o -E " ?[0-9]?[0-9]?[0-9]%")
     ;;
 
     *)
