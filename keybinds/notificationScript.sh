b09d83ec517b12d458b9aabbc9a9646e93d68311
Anthony Cieri <penguinmillion@gmail.com>
2023-01-30 00:09:39 -0800

    Add brightness and play/pause

--- keybinds/notificationScript.sh
+++ keybinds/notificationScript.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-DURATION=750
+DURATION=500
 
 case $1 in
     "mute")
         pactl set-sink-mute 3 toggle
-        notify-send -t $DURATION $(pactl get-sink-mute 3)
+        notify-send -t $DURATION "Volume" "$(pactl get-sink-mute 3)"
     ;;
 
     "v")
         pactl set-sink-volume 3 $2
-        notify-send -t $DURATION $(pactl get-sink-volume 3 | grep -o -E " ?[0-9]?[0-9]?[0-9]%")
+        notify-send -t $DURATION "Volume" "$(pactl get-sink-volume 3 | grep -o -E " ?[0-9]?[0-9]?[0-9]%")"
+    ;;
+
+    "b")
+        brightnessctl -d intel_backlight s $2
+        notify-send -t $DURATION Brightness "$(($(brightnessctl get) / 960))%"
     ;;
 
     *)
