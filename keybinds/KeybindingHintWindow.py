4c345a7f00481037da568a579469565f006410e0
Anthony Cieri <penguinmillion@gmail.com>
2023-02-09 18:18:05 -0800

    initial window

--- keybinds/KeybindingHintWindow.py
+++ keybinds/KeybindingHintWindow.py
@@ -1,19 +1,76 @@
 import gi
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+symbolDict = {
+    "$mainMod" : "Super"
+}
+
+
+appBinds = [
+    (["$mainMod"], "E", "nemo"),
+    (["$mainMod"], "R", "runner"),
+    # (["$mainMod"], "T", ""),
+    # (["$mainMod"], "Y", ""),
+    (["$mainMod"], "U", "Math122"),
+    (["$mainMod"], "I", "Math101"),
+    (["$mainMod"], "O", "ATWP"),
+    (["$mainMod"], "P", "CSC115"),
+
+
+    (["$mainMod"], "A", "VSCode"),
+    (["$mainMod"], "S", "Gedit"),
+    (["$mainMod"], "D", "Calculator"),
+    # (["$mainMod"], "F", "")
+
+    (["$mainMod"], "G", "Pearson"),
+    (["$mainMod"], "H", "Brightspace"),
+    (["$mainMod"], "j", "Wikipedia"),
+    (["$mainMod"], "K", "Desmos"),
+    (["$mainMod"], "L", "Firefox")
+]
+
+categories = ["Apps", "Window", "Workspace", "System"]
+bindingLists = [appBinds, appBinds, appBinds, appBinds]
 
 class MyWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Hello World")
+        super().__init__(title="Keybinds")
+        width = 100
+        height = 2
 
-        self.button = Gtk.Button(label="Click Here")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
+        self.grid = Gtk.Grid()
 
-    def on_button_clicked(self, widget):
-        print("Hello World")
+        for i in range(len(categories)):
+            newGrid = Gtk.Grid()
+
+            bindingList = bindingLists[i]
+            for j in range(len(bindingList)):
+                bindings = bindingList[j]
+                mod = symbolDict[bindings[0][0]]
+                key = bindings[1]
+                action = bindings[2]
+
+                accStr = mod + " + " + key + " : " + action
+
+                newLabel = Gtk.Label(label=accStr)
+                newGrid.attach(newLabel, 0, j * height, width, height)
+            
+            newFrame = Gtk.Frame(child=newGrid, label=categories[i])
+            
+            self.grid.attach(newFrame, i * width, 0, width, height)
+
+
+        # self.grid.add()
+        # for frame in self.frameList:
+        #     self.grid.add(frame)
+
+        # self.winL = Gtk.Label(label="hello")
+        # self.add(self.winL)
+
+        # self.VSep = Gtk.separator()
+
+        self.add(self.grid)
 
 
 win = MyWindow()
